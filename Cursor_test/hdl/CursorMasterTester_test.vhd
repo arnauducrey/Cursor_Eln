275c461ca3c429763a42b5a037aa093bbc24a7d8
arnauducrey <ducreyarn@gmail.com>
2021-12-10 13:30:45 +0100

    machine d'état fonctionnelle

--- Cursor_test/hdl/CursorMasterTester_test.vhd
+++ Cursor_test/hdl/CursorMasterTester_test.vhd
@@ -27,11 +27,20 @@ BusBouton <= "0000",
 			 "0010" after 40 us,
 			 "0000" after 45 us,
 			 "0100" after 100 us,
-			 "0000" after 105 us;
+			 "0000" after 105 us,
+			 "1000" after 140 us,
+			 "0000" after 145 us,
+			 "0100" after 160 us,
+			 "0000" after 165 us,
+			 "0010" after 200 us,
+			 "0000" after 205 us,
+			 "1000" after 240 us;
 				
 sensor1 <= 	'0',
 			'1' after 25 us,
-			'0' after 30 us;
+			'0' after 30 us,
+			'1' after 150 us,
+			'0' after 155 us;
 			
 STOP_Acc_dec <= '0',
 				'1' after 60 us,
@@ -39,10 +48,23 @@ STOP_Acc_dec <= '0',
 				'1' after 80 us,
 				'0' after 90 us,
 				'1' after 110 us,
-				'0' after 115 us;
+				'0' after 115 us,
+				'1' after 130 us,
+				'0' after 135 us,
+				'1' after 170 us,
+				'0' after 175 us,
+				'1' after 190 us,
+				'0' after 195 us,
+				'1' after 210 us,
+				'0' after 215 us,
+				'1' after 230 us,
+				'0' after 235 us;
 
 position <= to_unsigned (0,16),
-			to_unsigned (28572,16) after 70 us;
+			to_unsigned (28572,16) after 70 us,
+			to_unsigned (2857,16) after 120 us,
+			to_unsigned(17143,16) after 180 us,
+			to_unsigned(2857,16) after 220 us;
 
 
 END ARCHITECTURE test;
